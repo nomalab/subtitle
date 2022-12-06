f934b17243c9692f1868005976fbcb9b69db8de3
paul <paul@oupen.fr>
2022-12-06 18:08:26 +0100

    draft paul work

--- src/main/java/fr/noop/subtitle/ass/AssParser.java
+++ src/main/java/fr/noop/subtitle/ass/AssParser.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.HashMap;
 
 import fr.noop.subtitle.model.SubtitleObject;
 import fr.noop.subtitle.model.SubtitleLine;
@@ -14,6 +15,7 @@ import fr.noop.subtitle.model.SubtitleParser;
 import fr.noop.subtitle.model.SubtitleParsingException;
 import fr.noop.subtitle.util.*;
 import fr.noop.subtitle.util.SubtitleStyle.TextAlign;
+import fr.noop.subtitle.util.SubtitleTextLine;
 
 public class AssParser implements SubtitleParser {
 	private String charset; // Charset of the input files
@@ -95,6 +97,7 @@ public class AssParser implements SubtitleParser {
 								"Format Don't exist !"));
 					}
 					SubtitleStyle style = new SubtitleStyle();
+					Map<String, String> stylesAss = new HashMap<String, String>();
 
 					style = parseStyleForASS(line.split(":")[1].trim().split(","), styleFormat, line, isASS);
 
@@ -125,6 +128,7 @@ public class AssParser implements SubtitleParser {
 					if (dialogueFormat.size() == 0) {
 						// mettre une erreur
 					}
+
 					/**********************
 					 * Utiliser la variable cue et les fonction addLine ou setLines et
 					 * assObject.addCue(cue) pour remplacer ça :
@@ -143,6 +147,10 @@ public class AssParser implements SubtitleParser {
 					 * assObject.captions.put(key, caption);
 					 * assObject.captions.put(key, caption);
 					 ************************/
+					SubtitleTextLine line.split(":",2)[1].trim().split(",",dialogueFormat.length) = new SubtitleTextLine();
+					line.addText(new SubtitlePlainText(text));
+					cue.addLine(line);
+
 				}
 				continue;
 			}
@@ -152,74 +160,65 @@ public class AssParser implements SubtitleParser {
 	}
 
 	private Style parseStyleForASS(String[] line, String[] styleFormat, int index, boolean isASS) {
-		Map<String, String> stylesAss = new HashMap<String, String>();
-		String  name = "";
-		Style newStyle = new Style(Style.defaultID());
-		if (line.length != styleFormat.length){
-			//both should have the same size
+		String name = "";
+		SubtitleStyle style = new SubtitleStyle();
+		if (line.length != styleFormat.length) {
+			// both should have the same size
 		} else {
 			for (int i = 0; i < styleFormat.length; i++) {
 				String trimmedStyleFormat = styleFormat[i].trim();
 				if (trimmedStyleFormat.equalsIgnoreCase("Name")) {
-					  name =  line[i].trim();
+					name = line[i].trim();
 				}
-				//we go through every format parameter and save the interesting values
-				else if (trimmedStyleFormat.equalsIgnoreCase("PrimaryColour")){
-					//we save the color
-					String color =line[i].trim();
-					if(isASS){
-						if(color.startsWith("&H")) newStyle.color=Style.getRGBValue("&HAABBGGRR", color);
-						else  newStyle.getProperty(SubtitleStyle.Property.COLOR).getRGBValue("decimalCodedAABBGGRR", color);
-,
-					} else {
-						if(color.startsWith("&H")) newStyle.color=Style.getRGBValue("&HBBGGRR", color);
-						else  newStyle.getProperty(SubtitleStyle.Property.COLOR) = Style.getRGBValue("decimalCodedBBGGRR", color);
-					}
-				}else if (trimmedStyleFormat.equalsIgnoreCase("Bold")){
-					//we save if bold
-					 newStyle.setProperty(SubtitleStyle.Property.FONT_WEIGHT, SubtitleStyle.FontWeight.BOLD).parseBoolean(line[i].trim());
+				// we go through every format parameter and save the interesting values
+				else if (trimmedStyleFormat.equalsIgnoreCase("PrimaryColour")) {
+					style.setProperty(SubtitleStyle.Property.COLOR, );
 
-				}else if (trimmedStyleFormat.equalsIgnoreCase("Italic")){
-					//we save if italic
-                    newStyle.setProperty(SubtitleStyle.Property.FONT_STYLE, SubtitleStyle.FontStyle.ITALIC).parseBoolean(line[i].trim());
+				} else if (trimmedStyleFormat.equalsIgnoreCase("Bold")) {
+					// we save if bold
+					style.setProperty(SubtitleStyle.Property.FONT_WEIGHT, SubtitleStyle.FontWeight.BOLD);
 
-				}else if (trimmedStyleFormat.equalsIgnoreCase("Underline")){
-					//we save if underlined
-					 newStyle.setProperty(SubtitleStyle.Property.TEXT_DECORATION, SubtitleStyle.TextDecoration.UNDERLINE).parseBoolean(line[i].trim());
+				} else if (trimmedStyleFormat.equalsIgnoreCase("Italic")) {
+					// we save if italic
+					style.setProperty(SubtitleStyle.Property.FONT_STYLE, SubtitleStyle.FontStyle.ITALIC);
 
-				}else if (trimmedStyleFormat.equalsIgnoreCase("Alignment")){
-					//we save the alignment
-					int placement =Integer.parseInt(line[i].trim());
-					if (isASS){
-						switch(placement){
-						case 1:
-							newStyle.setTextAlign(TextAlign.LEFT);
-							break;
-						case 2:
-							newStyle.setTextAlign(TextAlign.CENTER);
-							break;
-						case 3:
-							newStyle.setTextAlign(TextAlign.RIGHT);
-							break;
+				} else if (trimmedStyleFormat.equalsIgnoreCase("Underline")) {
+					// we save if underlined
+					style.setProperty(SubtitleStyle.Property.TEXT_DECORATION, SubtitleStyle.TextDecoration.UNDERLINE);
+
+				} else if (trimmedStyleFormat.equalsIgnoreCase("Alignment")) {
+					// we save the alignment
+					int placement = Integer.parseInt(line[i].trim());
+					if (isASS) {
+						switch (placement) {
+							case 1:
+								style.setTextAlign(TextAlign.LEFT);
+								break;
+							case 2:
+								style.setTextAlign(TextAlign.CENTER);
+								break;
+							case 3:
+								style.setTextAlign(TextAlign.RIGHT);
+								break;
 						}
 					} else {
-						switch(placement){
-						case 9:
-							newStyle.setTextAlign(TextAlign.LEFT);
-							break;
-						case 10:
-							newStyle.setTextAlign(TextAlign.CENTER);
-							break;
-						case 11:
-							newStyle.setTextAlign(TextAlign.RIGHT);
-							break;
+						switch (placement) {
+							case 9:
+								style.setTextAlign(TextAlign.LEFT);
+								break;
+							case 10:
+								style.setTextAlign(TextAlign.CENTER);
+								break;
+							case 11:
+								style.setTextAlign(TextAlign.RIGHT);
+								break;
 						}
 					}
-				}	
-				
+				}
+
 			}
 		}
-		stylesAss.put(name, newStyle);
-		return newStyle;
+		stylesAss.put(name, style);
+		return style;
 	}
 }
