795c46bcb8c94ba6cd60e870750a4906731712d9
Martial Brasselet <martial@nomalab.com>
2026-05-26 17:48:33 +0200

    replace subtitle text line utils with base subtitle line class with voice property

--- src/main/java/fr/noop/subtitle/base/BaseSubtitleLine.java
+++ src/main/java/fr/noop/subtitle/base/BaseSubtitleLine.java
@@ -8,7 +8,11 @@
  *
  */
 
-package fr.noop.subtitle.util;
+package fr.noop.subtitle.base;
+
+/**
+ * Created by clebeaupin on 09/10/15.
+ */
 
 import fr.noop.subtitle.model.SubtitleLine;
 import fr.noop.subtitle.model.SubtitleText;
@@ -16,18 +20,18 @@ import fr.noop.subtitle.model.SubtitleText;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by clebeaupin on 06/10/15.
- */
-public class SubtitleTextLine implements SubtitleLine {
+public abstract class BaseSubtitleLine implements SubtitleLine {
     List<SubtitleText> texts;
+    String voice;
 
-    public SubtitleTextLine() {
+    public BaseSubtitleLine() {
         this.texts = new ArrayList<>();
+        this.voice = new String();
     }
 
-    public SubtitleTextLine(List<SubtitleText> texts) {
+    public BaseSubtitleLine(List<SubtitleText> texts, String voice) {
         this.texts = texts;
+        this.voice = voice;
     }
 
     public List<SubtitleText> getTexts() {
@@ -36,6 +40,14 @@ public class SubtitleTextLine implements SubtitleLine {
 
     public void addText(SubtitleText text) {
         this.texts.add(text);
+    }
+
+    public String getVoice() {
+        return this.voice;
+    }
+
+    public void setVoice(String voice) {
+        this.voice = voice;
     }
 
     public boolean isEmpty() {
